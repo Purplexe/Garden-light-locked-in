e0bf204a37a97322d15d6d6872d6024ccd712869
Purplexe <113139597+Purplexe@users.noreply.github.com>
2024-08-27 22:52:16 -0400

    Final changes :3

--- Assets/Activation.cs
+++ Assets/Activation.cs
@@ -10,7 +10,9 @@ public class Activation : MonoBehaviour
     public ParticleSystem particle;
 
 
-    // Start is called before the first frame update
+    
+   // Start is called before the first frame update
+   // get the player
     private void Start()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
@@ -22,15 +24,17 @@ public class Activation : MonoBehaviour
     {
         if (playerTransform != null)
         {
+            // If distance to player < the defined detection radius
             float distance = Vector3.Distance(transform.position, playerTransform.position);
             if (distance <= detection) 
             {
+                // Emit those particles
                 print("emitting");
                 var em = particle.emission;
                 em.enabled = true;
             }
         }
-        print(playerTransform.position);
+       
     }
    
 }

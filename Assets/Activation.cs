4924102a10e49adfdb4668d4f81ca235b46fcf40
Purplexe <113139597+Purplexe@users.noreply.github.com>
2024-08-27 22:48:04 -0400

    The thingie

--- Assets/Activation.cs
+++ Assets/Activation.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Activation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private Transform playerTransform;
+    [SerializeField] private float detection;
+    public ParticleSystem particle;
+
+
+    // Start is called before the first frame update
+    private void Start()
     {
-        
-    }
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
+    
 }
+
+    private void Update()
+    {
+        if (playerTransform != null)
+        {
+            float distance = Vector3.Distance(transform.position, playerTransform.position);
+            if (distance <= detection) 
+            {
+                print("emitting");
+                var em = particle.emission;
+                em.enabled = true;
+            }
+        }
+        print(playerTransform.position);
+    }
+   
+}
+
+
